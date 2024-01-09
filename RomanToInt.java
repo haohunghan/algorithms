3531b72a2d42ed0b74a1e7edd62494593525d2d7
haohunghan <anhhao18011997@gmail.com>
2024-01-09 23:11:47 +0700

    feat: roman to int

---
+++ RomanToInt.java
@@ -0,0 +1,119 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ *
+ * Example 2:
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ * Example 3:
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ */
+public class RomanToInt {
+
+    // sound like brute force
+    public static int romanToInt(String s) {
+        int result = 0;
+        Map<String, Integer> roman =  new HashMap<>();
+        roman.put("I", 1);
+        roman.put("V", 5);
+        roman.put("X", 10);
+        roman.put("L", 50);
+        roman.put("C", 100);
+        roman.put("D", 500);
+        roman.put("M", 1000);
+        roman.put("IV", 4);
+        roman.put("IX", 9);
+        roman.put("XL", 40);
+        roman.put("XC", 90);
+        roman.put("CD", 400);
+        roman.put("CM", 900);
+        if (s.length() == 1) {
+            return roman.get(s);
+        }
+
+        for (int i = 0; i < s.length();) {
+            if (i != s.length() - 1 && roman.containsKey(s.substring(i, i + 2))) {
+                result += roman.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                result += roman.get(s.substring(i, i + 1));
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+    public static int romanToIntBetter(String s) {
+
+        Map<String, Integer> roman =  new HashMap<>();
+        roman.put("I", 1);
+        roman.put("V", 5);
+        roman.put("X", 10);
+        roman.put("L", 50);
+        roman.put("C", 100);
+        roman.put("D", 500);
+        roman.put("M", 1000);
+        if (s.length() == 1) {
+            return roman.get(s);
+        }
+        int result = 0;
+        int prev=  0;
+        int number = 0;
+
+        for (int i = s.length() - 1; i >=0; i--) {
+            number = roman.get(String.valueOf(s.charAt(i)));
+            if (prev > number) {
+                result -= number;
+            } else {
+                result += number;
+            }
+            prev = number;
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToIntBetter("MCMXCIV"));
+    }
+}
