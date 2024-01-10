4bf8d3df18513abc6ea40193d522cbc1d8f9d16c
haohunghan <anhhao18011997@gmail.com>
2024-01-10 18:13:34 +0700

    feat: longest common prefix

---
+++ easy/LongestCommonPrefix.java
@@ -0,0 +1,48 @@
+package easy;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lowercase English letters.
+ */
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        String result = strs[0];
+        if (strs.length == 1) {
+            return result;
+        }
+
+        for (int i = 1; i < strs.length; i++) {
+            while(!strs[i].startsWith(result)) {
+                result = result.substring(0, result.length() - 1);
+                if (result.isEmpty()) {
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        String[] test = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(test));
+    }
+
+
+}
--- easy/Palindrome.java
+++ easy/Palindrome.java
@@ -1,3 +1,5 @@
+package easy;
+
 public class Palindrome {
     public static void main(String[] args) {
         System.out.println(isPalindrome(121));
